ddf183a5feda29651751ca5b41eea7670b8b72f5
Felix Pojtinger <pojntfx@localhost.localdomain>
2017-12-03 19:31:35 +0100

    Fixed typo

--- src/components/Grid/DashboardModulePlaceholder.vue
+++ src/components/Grid/DashboardModulePlaceholder.vue
@@ -8,13 +8,13 @@
       <br>
       <ol>
         <li>Create the .vue file for the module in /src/components/Vendors/YourOrganizationName/YourModuleName</li>
-        <li>Import the module into /src/router/index.js and add it to the routes you want it to be displayed on</li>
+        <li>Import the module into /src/router/index.js and add it to the routes you want it to be able to be displayed on</li>
         <li>
           Enable the dashboard layout editing mode by clicking on your avatar in the top left and add the module with the FAB in the bottom left (Not implemented yet)
           <br>
           <b>OR</b>
           <br>
-          Add an object like the examples in /src/store.js to /src/store.js with the same name that you've registered your module in /src/router/index.js.
+          Add an object like the placeholders in /src/store.js to /src/store.js with the same name that you've registered your module in /src/router/index.js with.
         </li>
       </ol>
       <b>Happy hacking!</b>
