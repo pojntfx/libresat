d5dfd4c877b369097daf82d084945cde784601cf
Felix Pojtinger <pojntfx@pojntfx-x230-antergos.fritz.box>
2017-12-10 23:29:22 +0100

    Added experimental module deletion functionality

--- src/components/Dash.vue
+++ src/components/Dash.vue
@@ -2,14 +2,20 @@
   <div>
     <!-- <b>From the parent: </b>{{ dashboardLayoutStart }} -->
     <opensnet-grid-template :layouts="dashboardLayoutStart" :editable="currentDashboardlayoutEditable"></opensnet-grid-template>
+    <v-btn @click="
+    deleteDashboardLayoutModeModule({group: 'start', layout: 1, /* mobile mode */ mode: 0, module: 0});
+    deleteDashboardLayoutModeModule({group: 'start', layout: 1, /* desktop mode */ mode: 1, module: 0})
+    ">
+      Delete first module in both modes (experimental)
+    </v-btn>
   </div>
 </template>
 
 <script>
 // Import the grid template to display the gridLayout
 import GridTemplate from './Grid/GridTemplate'
-// Import the helper getters for the editable dashboard layout mode from vuex
-import { mapGetters } from 'vuex'
+// Import the helper getters and actions for the editable dashboard layout mode from vuex
+import { mapGetters, mapActions } from 'vuex'
 export default {
   // Register the grid template component to be able to load it
   components: {
@@ -19,6 +25,11 @@ export default {
     ...mapGetters([
       'currentDashboardlayoutEditable',
       'dashboardLayoutStart'
+    ])
+  },
+  methods: {
+    ...mapActions([
+      'deleteDashboardLayoutModeModule'
     ])
   }
 }
