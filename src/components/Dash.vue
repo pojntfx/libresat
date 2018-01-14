28b64f73cd307dd2fb864a18432900abc756bcf3
Felicitas Pojtinger <felicitas@pojtinger.com>
2018-01-14 14:26:22 +0100

    Changed default grid config, removed "delete first module" button

--- src/components/Dash.vue
+++ src/components/Dash.vue
@@ -9,12 +9,6 @@
       :groupName="'start'"
       :layoutName="1"
     ></opensnet-grid-template>
-    <v-btn @click="
-    deleteDashboardLayoutModeModule({group: 'start', layout: 1, /* mobile mode */ mode: 0, module: 0});
-    deleteDashboardLayoutModeModule({group: 'start', layout: 1, /* desktop mode */ mode: 1, module: 0})
-    ">
-      Delete first module
-    </v-btn>
   </div>
 </template>
 
