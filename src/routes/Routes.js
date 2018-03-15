196c196796f605160d9a31b0fbccfa26fe0ecc3f
Felix Pojtinger <felix@pojtinger.com>
2018-03-15 15:09:56 +0100

    Added 404 handling

--- src/routes/Routes.js
+++ src/routes/Routes.js
@@ -1,7 +1,21 @@
 import React, { Component } from "react";
 
+// Router
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+
+// Routes
+import Start from "./Start";
+import PageNotFound from "./PageNotFound";
+
 export default class Routes extends Component {
   render() {
-    return <div>Hello, world!</div>;
+    return (
+      <Router>
+        <Switch>
+          <Route path="/" exact component={Start} />
+          <Route path="" component={PageNotFound} />
+        </Switch>
+      </Router>
+    );
   }
 }
