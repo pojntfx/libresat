edae9f0c4daa71f3af937c9e3c4ca70b13623a77
Felicitas Pojtinger <felicitas@pojtinger.com>
2018-03-26 01:05:02 +0200

    Fixed hero margins

--- components/home/Hero.js
+++ components/home/Hero.js
@@ -1,12 +1,12 @@
 export const Hero = () => (
-  <div className="jumbotron jumbotron-fluid bg-transparent text-light">
-    <h1 className="display-4 font-weight-light mt-5">
+  <div className="jumbotron jumbotron-fluid bg-transparent text-light py-0 pt-sm-5 pb-sm-5 pb-md-4">
+    <h1 className="display-4 font-weight-light mt-0 mt-md-5">
       Develop and Control Satellites, the Libre Way
     </h1>
-    <h2 className="display-5 font-weight-light mt-3 pb-4">
+    <h2 className="display-5 font-weight-light mt-3 pb-sm-5">
       The Open-Source Satellite Development and Control Platform
     </h2>
-    <p className="lead mt-4 mb-5">
+    <p className="lead mt-4 mb-0 mb-md-4">
       <a className="btn btn-primary" href="#" role="button">
         Get started
       </a>
