8c0778deb1daa04008d1d0a63d22f8a114dfd7c0
Felicitas Pojtinger <felicitas@pojtinger.com>
2018-03-26 00:51:55 +0200

    Added hero component, added translucent navbar background

---
+++ components/home/Hero.js
@@ -0,0 +1,18 @@
+export const Hero = () => (
+  <div className="jumbotron jumbotron-fluid bg-transparent text-light">
+    <h1 className="display-4 font-weight-light mt-5">
+      Develop and Control Satellites, the Libre Way
+    </h1>
+    <h2 className="display-5 font-weight-light mt-3 pb-4">
+      The Open-Source Satellite Development and Control Platform
+    </h2>
+    <p className="lead mt-4 mb-5">
+      <a className="btn btn-primary" href="#" role="button">
+        Get started
+      </a>
+      <a className="btn btn-secondary ml-3" href="#" role="button">
+        Watch video
+      </a>
+    </p>
+  </div>
+);
--- layouts/HomeLayout.js
+++ layouts/HomeLayout.js
@@ -1,17 +1,54 @@
-import { Fragment } from "react";
-import MainNavigation from "../components/MainNavigation";
+import { Component, Fragment } from "react";
+import { MainNavigation } from "../components/global/MainNavigation";
 import styled from "styled-components";
 
-export default ({ children }) => (
-  <Fragment>
-    <NavigationWrapper>
-      <MainNavigation />
-    </NavigationWrapper>
-    <div className="container pt-2">{children}</div>
-  </Fragment>
-);
+export class HomeLayout extends Component {
+  state = {
+    mainNavigationIsTransparent: true
+  };
 
-const NavigationWrapper = styled.header`
-  position: sticky;
-  top: 0;
+  handleScroll = () => {
+    window.scrollY > 477
+      ? this.setState({ mainNavigationIsTransparent: false })
+      : this.setState({ mainNavigationIsTransparent: true });
+  };
+
+  componentDidMount = () => {
+    // Check once at start
+    this.handleScroll();
+    // Check again when user has scrolled
+    window.addEventListener("scroll", this.handleScroll);
+  };
+
+  componentWillUnmount = () => {
+    window.removeEventListener("scroll");
+  };
+
+  render() {
+    const { children } = this.props;
+    const { mainNavigationIsTransparent } = this.state;
+
+    return (
+      <Fragment>
+        <HomeNavigation translucent={mainNavigationIsTransparent} />
+        <HomeContainer>{children}</HomeContainer>
+      </Fragment>
+    );
+  }
+}
+
+const HomeNavigation = styled(({ className }) => (
+  <header className={`bg-dark sticky-top ${className}`}>
+    <MainNavigation />
+  </header>
+))`
+  ${props =>
+    props.translucent
+      ? `background: inherit`
+      : `background: rgba(0, 0, 0, 0.9)!important`};
+  transition: background 0.2s ease-in-out;
 `;
+
+const HomeContainer = ({ children }) => (
+  <div className="container pt-2">{children}</div>
+);
