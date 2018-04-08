86615b383a7d4fb6aa6dfa25795506839cb65fab
Felix Pojtinger <felix@pojtinger.com>
2018-04-08 22:07:52 +0200

    Replaced global.css with injectGlobal, started theme implementation

--- src/index.js
+++ src/index.js
@@ -2,12 +2,30 @@ import React from "react";
 import ReactDOM from "react-dom";
 import registerServiceWorker from "./registerServiceWorker";
 
-// Normalize and global css
+// Normalize
 import "./assets/normalize.css";
-import "./assets/global.css";
+
+// Styled components
+import { ThemeProvider, injectGlobal } from "styled-components";
 
 // Routes
 import { Routes } from "./routes/Routes";
 
-ReactDOM.render(<Routes />, document.getElementById("osnet-bc"));
+// Style constants
+import * as theme from "./lib/constants";
+
+// Global styles
+injectGlobal`
+body {
+  font-family: Roboto, -apple-system, BlinkMacSystemFont, "Segoe UI", Oxygen-Sans,
+    Cantarell, Ubuntu, "Helvetica Neue", sans-serif;
+}
+`;
+
+ReactDOM.render(
+  <ThemeProvider theme={theme}>
+    <Routes />
+  </ThemeProvider>,
+  document.getElementById("osnet-bc")
+);
 registerServiceWorker();
