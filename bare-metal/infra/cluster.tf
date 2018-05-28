73d05a6e3b8cc7e30a400ab4ab8a61cbe4c68895
Felicitas Pojtinger <felicitas@pojtinger.com>
2018-05-28 17:15:54 +0200

    Added basic bare metal install docs

---
+++ bare-metal/infra/cluster.tf
@@ -0,0 +1,35 @@
+module "cluster" {
+  source = "git::https://github.com/poseidon/typhoon//bare-metal/container-linux/kubernetes?ref=v1.10.0"
+
+  providers = {
+    local = "local.default"
+    null = "null.default"
+    template = "template.default"
+    tls = "tls.default"
+  }
+
+  # bare-metal
+  cluster_name            = "${var.cluster_name}"
+  matchbox_http_endpoint  = "${var.matchbox_http_endpoint}"
+  container_linux_channel = "${var.container_linux_channel}"
+  container_linux_version = "${var.container_linux_version}"
+
+  # configuration
+  k8s_domain_name    = "${var.k8s_domain_name}"
+  ssh_authorized_key = "${var.ssh_authorized_key}"
+  asset_dir          = "${var.asset_dir}"
+
+  # machines
+  controller_names   = "${var.controller_names}"
+  controller_macs    = "${var.controller_macs}"
+  controller_domains = "${var.controller_domains}"
+  worker_names       = "${var.worker_names}"
+  worker_macs        = "${var.worker_macs}"
+  worker_domains     = "${var.worker_domains}"
+
+  # optional
+  networking          = "${var.networking}"
+  cached_install      = "${var.cached_install}"
+  install_disk        = "${var.install_disk}"
+  container_linux_oem = "${var.container_linux_oem}"
+}
