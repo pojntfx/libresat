c39e65c57446795015c243b8de73a2420c72df33
Felicitas Pojtinger <felicitas@pojtinger.com>
2018-08-06 23:28:54 +0200

    feat(libresat-forum Exim Mailman): add build script and exim config, data volume

---
+++ exim-smarthost-mailman/run.sh
@@ -0,0 +1,31 @@
+#! /bin/bash
+
+build_docker_image() {
+  docker build \
+  exim-smarthost-mailman \
+  -t $1
+  DOCKER_IMAGE_ID=$1
+}
+
+get_docker_container_id() {
+  DOCKER_CONTAINER_ID=$(docker run -d -h $1 $2)
+}
+
+send_test_mail() {
+  docker exec $1 bash -c "echo \"Test Message Body\" | mail -s \"Test Message Subject\" $2"
+}
+
+test_rest_api() {
+  # Sleep 10 seconds so that mailman can start
+  sleep 10
+  docker exec $1 bash -c "apt install -y curl && curl http://localhost:8001/3.1 && apt remove -y curl && apt -y autoremove"
+}
+
+echo "Building docker image ..."
+build_docker_image "libresat-forum-exim-mailman"
+echo "Starting docker container ..."
+get_docker_container_id "mail.libresat.space" $DOCKER_IMAGE_ID
+echo "Testing mail functionality ..."
+send_test_mail $DOCKER_CONTAINER_ID "goooglehupf007@gmail.com"
+echo "Testing REST API (should return 401) ..."
+test_rest_api $DOCKER_CONTAINER_ID
