67331c50c64864dbad4a95dd76cc0c1c861b64f5
Felix Pojtinger <felix@pojtinger.com>
2018-08-06 23:44:50 +0200

    feat(libresat-forum Exim Mailman): open up ports on docker container

--- exim-smarthost-mailman/run.sh
+++ exim-smarthost-mailman/run.sh
@@ -8,7 +8,7 @@ build_docker_image() {
 }
 
 get_docker_container_id() {
-  DOCKER_CONTAINER_ID=$(docker run -d -h $1 $2)
+  DOCKER_CONTAINER_ID=$(docker run -d -p 8001:8001 -h $1 $2)
 }
 
 send_test_mail() {
