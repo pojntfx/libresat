e4bea968deeb96ea997aeb97c0b4dd041b1db7b3
Felix Pojtinger <felix@pojtinger.com>
2018-08-15 21:56:37 +0200

    feat(libresat-forum): remove mail test as it does not work anymore due to mailman

--- mailman-suite/build-run-test.sh
+++ mailman-suite/build-run-test.sh
@@ -1,32 +1,26 @@
 #! /bin/bash
 
 build_docker_image() {
-  docker build \
-  mailman-suite \
-  -t $1
-  DOCKER_IMAGE_ID=$1
+	docker build \
+		mailman-suite \
+		-t $1
+	DOCKER_IMAGE_ID=$1
 }
 
 get_docker_container_id() {
-  DOCKER_CONTAINER_ID=$(docker run -d -p 25:25 -p 8000:80 -p 8024:8024 -h $1 $2)
-}
-
-send_test_mail() {
-  docker exec $1 bash -c "echo \"Test Message Body\" | mail -s \"Test Message Subject\" $2"
+	DOCKER_CONTAINER_ID=$(docker run -d -p 25:25 -p 8000:80 -p 8024:8024 -h $1 $2)
 }
 
 test_rest_api() {
-  # Sleep 15 seconds so that mailman can start
-  sleep 15
-  docker exec $1 bash -c "apt install -y curl && curl http://localhost:8001/3.1 && apt remove -y curl && apt -y autoremove"
+	# Sleep 80 seconds so that mailman can start
+	sleep 80
+	docker exec $1 bash -c "apt install -y curl && curl http://localhost:8001/3.1 && apt remove -y curl && apt -y autoremove"
 }
 
 echo "Building docker image ..."
 build_docker_image "libresat-forum"
 echo "Starting docker container ..."
 get_docker_container_id "mail.libresat.space" $DOCKER_IMAGE_ID
-echo "Testing mail functionality ..."
-send_test_mail $DOCKER_CONTAINER_ID "goooglehupf007@gmail.com"
 echo "Testing REST API (should return 401) ..."
 test_rest_api $DOCKER_CONTAINER_ID
 echo "Now open up http://localhost:8000/forum/hyperkitty and/or http://localhost:8000/forum/postorius to test out hyperkitty!"
