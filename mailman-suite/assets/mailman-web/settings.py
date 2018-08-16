6563ac98b3170e23752232a7a70d018364256788
Felicitas Pojtinger <felicitas@pojtinger.com>
2018-08-16 14:41:42 +0200

    feat(libresat-forum): add working volumes to containers

--- mailman-suite/assets/mailman-web/settings.py
+++ mailman-suite/assets/mailman-web/settings.py
@@ -93,7 +93,7 @@ WSGI_APPLICATION = 'wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'mailman-web.db'),
+        'NAME': os.path.join(BASE_DIR, 'databases', 'mailman-web.db'),
         'HOST': '',
     }
 }
