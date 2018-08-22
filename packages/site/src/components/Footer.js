8b9ce31f0987ec6e4997bdd76d8cbac0e8b91f59
Felix Pojtinger <felix@pojtinger.com>
2018-08-22 18:35:59 +0200

    feat(site): improve add basic license and imprint modals

--- packages/site/src/components/Footer.js
+++ packages/site/src/components/Footer.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Segment, Grid } from "semantic-ui-react";
+import { Segment, Grid, Modal, Button } from "semantic-ui-react";
 import styled from "styled-components";
 
 const FooterWrapper = styled(Segment)`
@@ -7,19 +7,65 @@ const FooterWrapper = styled(Segment)`
   margin-bottom: 1em !important;
 `;
 
+const FooterSection = styled(Grid.Column)`
+  display: flex !important;
+  align-items: center;
+  justify-content: start;
+  &:last-child {
+    justify-content: end;
+  }
+  @media (max-width: 767px) {
+    justify-content: start !important;
+  }
+`;
+
+const MetaButton = styled(Button)`
+  white-space: nowrap;
+`;
+
 const FooterContent = ({ licenses, imprint }) => (
   <FooterWrapper>
-    <Grid stackable colums={2}>
-      <Grid.Column>
+    <Grid stackable columns={2}>
+      <FooterSection width={4}>
         <span>
           &copy; {licenses.media.year} {licenses.media.holder}
         </span>
-      </Grid.Column>
+      </FooterSection>
+      <FooterSection width={12}>
+        <Grid stackable columns={3}>
+          <Grid.Column>
+            <Modal
+              trigger={
+                <MetaButton fluid icon="images" content="Media License" />
+              }
+              header="Media License"
+              content="Text Here"
+              closeIcon
+            />
+          </Grid.Column>
+          <Grid.Column>
+            <Modal
+              trigger={<MetaButton fluid icon="code" content="Code License" />}
+              header="Code License"
+              content="Text Here"
+              closeIcon
+            />
+          </Grid.Column>
+          <Grid.Column>
+            <Modal
+              trigger={<MetaButton fluid icon="legal" content="Imprint" />}
+              header="Imprint"
+              content="Text Here"
+              closeIcon
+            />
+          </Grid.Column>
+        </Grid>
+      </FooterSection>
     </Grid>
   </FooterWrapper>
 );
 
-const config = {
+const footerConfig = {
   licenses: {
     media: {
       spdxLicenseIdentifier: "CC-BY-SA-4.0",
@@ -32,5 +78,5 @@ const config = {
 };
 
 export const Footer = props => (
-  <FooterContent licenses={config.licenses} {...props} />
+  <FooterContent licenses={footerConfig.licenses} {...props} />
 );
