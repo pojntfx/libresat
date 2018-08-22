acde7b92358c21cd7129f36df741876a0466b143
Felix Pojtinger <felix@pojtinger.com>
2018-08-22 14:42:01 +0200

    feat(site): enable blog, add MDX demos

--- packages/site/gatsby-config.js
+++ packages/site/gatsby-config.js
@@ -4,8 +4,24 @@ module.exports = {
     {
       resolve: "gatsby-source-filesystem",
       options: {
-        name: "src",
+        name: "data",
         path: `${__dirname}/src/data`
+      }
+    },
+    {
+      resolve: "gatsby-mdx",
+      options: {
+        defaultLayouts: {
+          posts: require.resolve("./src/layouts/Post.js"),
+          default: require.resolve("./src/layouts/Default.js")
+        }
+      }
+    },
+    {
+      resolve: "gatsby-source-filesystem",
+      options: {
+        name: "posts",
+        path: `${__dirname}/src/posts/`
       }
     },
     "gatsby-plugin-styled-components"
