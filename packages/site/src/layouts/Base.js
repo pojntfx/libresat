85bda96154e90b0a55048b6827b612d66af8d6da
Felix Pojtinger <felix@pojtinger.com>
2018-08-22 18:44:30 +0200

    feat(site): add background image

--- packages/site/src/layouts/Base.js
+++ packages/site/src/layouts/Base.js
@@ -3,6 +3,17 @@ import "semantic-ui-css/semantic.min.css";
 import { Container } from "semantic-ui-react";
 import { Navbar } from "../components/Navbar";
 import { Footer } from "../components/Footer";
+import { injectGlobal } from "styled-components";
+import { withPrefix } from "gatsby-link";
+
+injectGlobal`
+  body {
+    background: url("${withPrefix(
+      "/img/bg.jpg"
+    )}") no-repeat center center fixed;
+    background-size: cover;
+  }
+`;
 
 export const Base = ({ children, ...otherProps }) => (
   <>
