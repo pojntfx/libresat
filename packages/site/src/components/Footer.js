49462f709478855ebb1721017d8ebd931737a8fd
Felicitas Pojtinger <felicitas@pojtinger.com>
2018-08-22 23:01:26 +0200

    feat(site): add media license info to footer

--- packages/site/src/components/Footer.js
+++ packages/site/src/components/Footer.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { Segment, Grid, Modal, Button } from "semantic-ui-react";
+import { StaticQuery, graphql } from "gatsby";
 import styled from "styled-components";
 
 const FooterWrapper = styled(Segment)`
@@ -23,12 +24,46 @@ const MetaButton = styled(Button)`
   white-space: nowrap;
 `;
 
-const FooterContent = ({ licenses, imprint }) => (
+const LicenseBadgeImg = styled.img`
+  padding-bottom: 1em;
+`;
+
+const LicenseBadge = ({ src, alt, href, ...otherProps }) => (
+  <a href={href}>
+    <LicenseBadgeImg src={src} alt={alt} {...otherProps} />
+  </a>
+);
+
+const LicenseInfo = ({
+  spdxLicenseIdentifier,
+  product,
+  copyrightHolder,
+  licenseText,
+  timeSpan,
+  badge,
+  moreInfo
+}) => (
+  <Modal.Content>
+    <LicenseBadge src={badge.src} alt={badge.alt} href={moreInfo.link} />
+    <p>
+      {product} &copy; {timeSpan} {copyrightHolder}
+    </p>
+    <span>SPDX-License-Identifier: {spdxLicenseIdentifier}</span>
+    <br />
+    <p>{licenseText}</p>
+    <p>
+      Find out more about this license over at{" "}
+      <a href={moreInfo.link}>{moreInfo.text}</a>.
+    </p>
+  </Modal.Content>
+);
+
+const FooterContent = ({ licenses: { media }, imprint }) => (
   <FooterWrapper>
     <Grid stackable columns={2}>
       <FooterSection width={4}>
         <span>
-          &copy; {licenses.media.year} {licenses.media.holder}
+          &copy; {media.timeSpan} {media.copyrightHolder}
         </span>
       </FooterSection>
       <FooterSection width={12}>
@@ -38,18 +73,28 @@ const FooterContent = ({ licenses, imprint }) => (
               trigger={
                 <MetaButton fluid icon="images" content="Media License" />
               }
-              header="Media License"
-              content="Text Here"
               closeIcon
-            />
+            >
+              <Modal.Header>Media License</Modal.Header>
+              <LicenseInfo
+                spdxLicenseIdentifier={media.spdxLicenseIdentifier}
+                product={media.product}
+                copyrightHolder={media.copyrightHolder}
+                licenseText={media.licenseText}
+                timeSpan={media.timeSpan}
+                badge={media.badge}
+                moreInfo={media.moreInfo}
+              />
+            </Modal>
           </Grid.Column>
           <Grid.Column>
             <Modal
               trigger={<MetaButton fluid icon="code" content="Code License" />}
-              header="Code License"
-              content="Text Here"
               closeIcon
-            />
+            >
+              <Modal.Header>Code License</Modal.Header>
+              asdf
+            </Modal>
           </Grid.Column>
           <Grid.Column>
             <Modal
@@ -65,18 +110,35 @@ const FooterContent = ({ licenses, imprint }) => (
   </FooterWrapper>
 );
 
-const footerConfig = {
-  licenses: {
-    media: {
-      spdxLicenseIdentifier: "CC-BY-SA-4.0",
-      badgeUrl: "https://licensebuttons.net/l/by-sa/4.0/88x31.png",
-      product: "LibreSat Website",
-      holder: "Felicitas Pojtinger",
-      year: "2018"
-    }
-  }
-};
+export const FooterView = ({ data, ...otherProps }) => (
+  <FooterContent licenses={data.siteYaml.licenses} {...otherProps} />
+);
 
-export const Footer = props => (
-  <FooterContent licenses={footerConfig.licenses} {...props} />
+export const Footer = () => (
+  <StaticQuery
+    query={graphql`
+      query FooterQuery {
+        siteYaml {
+          licenses {
+            media {
+              spdxLicenseIdentifier
+              product
+              copyrightHolder
+              licenseText
+              timeSpan
+              badge {
+                alt
+                src
+              }
+              moreInfo {
+                text
+                link
+              }
+            }
+          }
+        }
+      }
+    `}
+    render={data => <FooterView data={data} />}
+  />
 );
