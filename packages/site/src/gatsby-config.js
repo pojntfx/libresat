fec880576334797c4e0d3377032d065f9735aa5c
Felicitas Pojtinger <felicitas@pojtinger.com>
2018-08-22 12:19:48 +0200

    feat(site): add gatsby, semantic ui and styled-components

---
+++ packages/site/src/gatsby-config.js
@@ -0,0 +1,5 @@
+export const plugins = [
+  {
+    resolve: "gatsby-plugin-styled-components"
+  }
+];
---
+++ packages/site/src/pages/index.js
@@ -0,0 +1,8 @@
+import React from "react";
+import { DefaultLayout } from "../layouts/default";
+
+export default () => (
+  <DefaultLayout>
+    <p>Hello, World!</p>
+  </DefaultLayout>
+);
