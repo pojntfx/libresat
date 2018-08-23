528e241f741eecb3fc280816d395d5b71fa33ae2
Felix Pojtinger <felix@pojtinger.com>
2018-08-23 12:07:06 +0200

    feat(site): add basic card mockup to index

--- packages/site/src/pages/index.js
+++ packages/site/src/pages/index.js
@@ -1,7 +1,16 @@
 import React from "react";
 import { Base } from "../layouts/Base";
 import styled from "styled-components";
-import { Button, Modal } from "semantic-ui-react";
+import {
+  Button,
+  Modal,
+  Card,
+  Grid,
+  Header,
+  Divider,
+  Icon,
+  Statistic
+} from "semantic-ui-react";
 import Link from "gatsby-link";
 import { withPrefix } from "gatsby";
 
@@ -63,6 +72,102 @@ const Hero = ({ title, subtitle, primaryAction, secondaryAction }) => (
   </HeroWrapper>
 );
 
+const CardAction = styled(Button)`
+  white-space: nowrap;
+`;
+
+const CardActions = styled(Grid.Column).attrs({
+  width: 10
+})`
+  display: flex !important;
+  overflow-x: auto;
+  & > ${CardAction}:first-child {
+    margin-left: auto;
+    @media (max-width: 767px) {
+      margin-left: auto;
+    }
+  }
+  & > ${CardAction}:last-child {
+    @media (max-width: 767px) {
+      margin-right: auto;
+    }
+  }
+`;
+
+const SoftwareCardHeader = () => (
+  <Grid stackable colums={2}>
+    <Grid.Column width={6}>
+      <Header
+        content="Software"
+        subheader="Liberate your Spacecraft"
+        icon="code"
+      />
+    </Grid.Column>
+    <CardActions>
+      <CardAction color="blue" content="Read the docs" icon="book" />
+      <CardAction content="Contribute" icon="fork" />
+      <CardAction content="Discuss" icon="conversation" />
+    </CardActions>
+  </Grid>
+);
+
+const SoftwareCardDescription = styled(Card.Description)`
+  padding: 1em 0;
+`;
+
+const SoftwareCardOverviewIcon = styled(Icon)`
+  padding-right: 1.75em;
+`;
+
+const SoftwareCardOverview = () => (
+  <>
+    <Divider horizontal>
+      <SoftwareCardOverviewIcon name="compass" />
+      Overview
+    </Divider>
+    <Grid stackable divided="vertically" columns={3} textAlign="center">
+      <Grid.Column>
+        <Statistic>
+          <Statistic.Value>10024</Statistic.Value>
+          <Statistic.Label>
+            <Icon name="file code" /> Lines of Code
+          </Statistic.Label>
+        </Statistic>
+      </Grid.Column>
+      <Grid.Column>
+        <Statistic>
+          <Statistic.Value>1</Statistic.Value>
+          <Statistic.Label>
+            <Icon name="users" /> Contributor
+          </Statistic.Label>
+        </Statistic>
+      </Grid.Column>
+      <Grid.Column>
+        <Statistic>
+          <Statistic.Value>AGPL</Statistic.Value>
+          <Statistic.Label>
+            <Icon name="lock open" /> Licensed
+          </Statistic.Label>
+        </Statistic>
+      </Grid.Column>
+    </Grid>
+  </>
+);
+
+const SoftwareCard = () => (
+  <Card fluid color="blue">
+    <Card.Content>
+      <SoftwareCardHeader />
+      <SoftwareCardDescription>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat, ullam
+        similique? Maxime laborum blanditiis repellendus facere fuga alias rerum
+        minima!
+      </SoftwareCardDescription>
+      <SoftwareCardOverview />
+    </Card.Content>
+  </Card>
+);
+
 export default () => (
   <Base>
     <Hero
@@ -80,5 +185,6 @@ export default () => (
         videoSrc: "http://lunduke.com/720classic/LinuxSucks-2016.mp4"
       }}
     />
+    <SoftwareCard />
   </Base>
 );
