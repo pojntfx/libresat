471202d5d548bddca36533bd35579b4b99bd2e88
Felix Pojtinger <felix@pojtinger.com>
2018-08-25 21:52:49 +0200

    feat(site): start code splitting and prepare for fetching data from MDX

---
+++ packages/site/src/components/Blog/GallerySection.js
@@ -0,0 +1,40 @@
+import React from "react";
+import styled from "styled-components";
+import { Card } from "semantic-ui-react";
+import Link, { withPrefix } from "gatsby-link";
+import { Gallery } from "./Gallery";
+
+const LatestPostTemplate = props => (
+  <Card
+    fluid
+    image={withPrefix("/img/software-card.png")}
+    header="Post Title"
+    meta="2 days ago by Felix Pojtinger. Last edit 1 day ago. Estimated reading time: 1 Minute."
+    description={`Waaayyyy longeeer Post Description ${Math.random()
+      .toString(36)
+      .substring(7)}`}
+    {...props}
+  />
+);
+
+const LatestPost = styled(LatestPostTemplate)`
+  margin-top: 1em !important;
+  margin-bottom: 1em !important;
+  width: auto !important;
+  max-width: ${1127 / 2 - 15}px !important;
+  @media (max-width: 1127px) {
+    max-width: 100% !important;
+    width: 100% !important;
+  }
+`;
+
+export const GallerySection = () => (
+  <Gallery options={{ gutter: 30 }}>
+    <LatestPost as={Link} to={withPrefix("/")} />
+    <LatestPost as={Link} to={withPrefix("/")} />
+    <LatestPost as={Link} to={withPrefix("/")} />
+    <LatestPost as={Link} to={withPrefix("/")} />
+    <LatestPost as={Link} to={withPrefix("/")} />
+    <LatestPost as={Link} to={withPrefix("/")} />
+  </Gallery>
+);
--- packages/site/src/pages/blog.js
+++ packages/site/src/pages/blog.js
@@ -1,95 +1,15 @@
 import React from "react";
 import { Base } from "../layouts/Base";
-import { Container, Card, Header } from "semantic-ui-react";
+import { Container, Header } from "semantic-ui-react";
 import { Paper } from "../components/Paper";
-import CoverflowTemplate from "reactjs-coverflow";
-import { withPrefix } from "gatsby";
-import Link from "gatsby-link";
-import styled from "styled-components";
-import MasonryTemplate from "react-masonry-component";
-
-const FeaturedPostTemplate = props => (
-  <Card
-    image={withPrefix("/img/software-card.png")}
-    header="Post Title"
-    meta="2 days ago by Felix Pojtinger"
-    description="Post Description"
-    {...props}
-  />
-);
-
-const FeaturedPost = styled(FeaturedPostTemplate)`
-  margin-top: 1em !important;
-  margin-bottom: 1em !important;
-`;
-
-const Coverflow = styled(CoverflowTemplate)`
-  margin-top: 1em;
-  margin-bottom: 1em;
-`;
-
-const CoverflowSection = () => (
-  <Coverflow rotate={10}>
-    <FeaturedPost as={Link} to={withPrefix("/")} />
-    <FeaturedPost as={Link} to={withPrefix("/")} />
-    <FeaturedPost as={Link} to={withPrefix("/")} />
-    <FeaturedPost as={Link} to={withPrefix("/")} />
-    <FeaturedPost as={Link} to={withPrefix("/")} />
-  </Coverflow>
-);
-
-const LatestPostTemplate = props => (
-  <Card
-    fluid
-    image={withPrefix("/img/software-card.png")}
-    header="Post Title"
-    meta="2 days ago by Felix Pojtinger. Last edit 1 day ago. Estimated reading time: 1 Minute."
-    description={`Waaayyyy longeeer Post Description ${Math.random()
-      .toString(36)
-      .substring(7)}`}
-    {...props}
-  />
-);
-
-const LatestPost = styled(LatestPostTemplate)`
-  margin-top: 1em !important;
-  margin-bottom: 1em !important;
-  width: auto !important;
-  max-width: ${1127 / 2 - 15}px !important;
-  @media (max-width: 1127px) {
-    max-width: 100% !important;
-    width: 100% !important;
-  }
-`;
-
-const Masonry = styled(MasonryTemplate)`
-  margin-top: -1em;
-  margin-bottom: -1em;
-`;
-
-const Gallery = () => (
-  <Masonry options={{ gutter: 30 }}>
-    <LatestPost as={Link} to={withPrefix("/")} />
-    <LatestPost as={Link} to={withPrefix("/")} />
-    <LatestPost as={Link} to={withPrefix("/")} />
-    <LatestPost as={Link} to={withPrefix("/")} />
-    <LatestPost as={Link} to={withPrefix("/")} />
-    <LatestPost as={Link} to={withPrefix("/")} />
-  </Masonry>
-);
-
-const BlogHeader = styled(Header).attrs({
-  inverted: true,
-  textAlign: "center"
-})`
-  padding-top: 4em !important;
-  padding-bottom: 4em !important;
-`;
+import { PageHeader } from "../components/Blog/PageHeader";
+import { CoverflowSection } from "../components/Blog/CoverflowSection";
+import { GallerySection } from "../components/Blog/GallerySection";
 
 export default () => (
   <Base noContainer>
     <Container>
-      <BlogHeader
+      <PageHeader
         as="h1"
         content="The LibreSat Blog"
         subheader="Posts on all things LibreSat."
@@ -113,7 +33,7 @@ export default () => (
           Consider contributing your own post by submitting a merge request!`}
         />
       </Paper>
-      <Gallery />
+      <GallerySection />
     </Container>
   </Base>
 );
