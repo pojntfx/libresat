c83f9217cb3099df79bc231d306b3e3b243efe08
Felicitas Pojtinger <felicitas@pojtinger.com>
2018-08-25 11:21:22 +0200

    feat(site): add highlighted parts section to home

---
+++ packages/site/src/components/Home/HighlightedParts.js
@@ -0,0 +1,40 @@
+import React from "react";
+import { Card, Header, Icon } from "semantic-ui-react";
+import styled from "styled-components";
+import Link from "gatsby-link";
+
+const Description = styled(Card.Description)`
+  margin-bottom: 1.25em;
+`;
+
+const Links = ({ links, linksPerRow, ...otherProps }) => (
+  <Card.Group stackable itemsPerRow={linksPerRow} {...otherProps}>
+    {links.map(({ label, icon, link, description }, index) => (
+      <Card as={Link} to={link} fluid key={index}>
+        <Card.Content>
+          <Header as="h2" textAlign="center" icon>
+            <Icon name={icon} />
+            {label}
+            <Header.Subheader>{description}</Header.Subheader>
+          </Header>
+        </Card.Content>
+      </Card>
+    ))}
+  </Card.Group>
+);
+
+export const HighlightedParts = ({
+  header,
+  description,
+  linksPerRow,
+  links,
+  ...otherProps
+}) => (
+  <Card fluid {...otherProps}>
+    <Card.Content>
+      <Header textAlign="center" {...header} />
+      <Description>{description}</Description>
+      <Links links={links} linksPerRow={linksPerRow} />
+    </Card.Content>
+  </Card>
+);
