a0757150401af18df297724e598ea889e2a8ad8d
Felicitas Pojtinger <felicitas@pojtinger.com>
2018-08-25 20:21:20 +0200

    feat(site): add masonry grid for latest posts, add icons to headers in blog

--- packages/site/src/pages/blog.js
+++ packages/site/src/pages/blog.js
@@ -1,11 +1,12 @@
-import React, { Component } from "react";
+import React from "react";
 import { Base } from "../layouts/Base";
-import { Container, Card } from "semantic-ui-react";
+import { Container, Card, Header } from "semantic-ui-react";
 import { Paper } from "../components/Paper";
 import CoverflowTemplate from "reactjs-coverflow";
 import { withPrefix } from "gatsby";
 import Link from "gatsby-link";
 import styled from "styled-components";
+import MasonryTemplate from "react-masonry-component";
 
 const FeaturedPostTemplate = props => (
   <Card
@@ -37,31 +38,69 @@ const CoverflowSection = () => (
   </Coverflow>
 );
 
-export default class Blog extends Component {
-  render() {
-    return (
-      <Base noContainer>
-        <Container>
-          <Paper>
-            <h1>Featured Posts</h1>
-            <p>
-              Check out the featured, editorial posts below and get an insight
-              into recent news, updates and changes.
-            </p>
-          </Paper>
-        </Container>
-        <CoverflowSection />
-        <Container>
-          <Paper>
-            <h1>Latest Posts</h1>
-            <p>
-              Non-editorial, federated posts by the community and/or core devs.
-              Consider contributing your own article by submitting a merge
-              request!
-            </p>
-          </Paper>
-        </Container>
-      </Base>
-    );
+const LatestPostTemplate = props => (
+  <Card
+    fluid
+    image={withPrefix("/img/software-card.png")}
+    header="Post Title"
+    meta="2 days ago by Felicitas Pojtinger. Last edit 1 day ago. Estimated reading time: 1 Minute."
+    description={`Waaayyyy longeeer Post Description ${Math.random()
+      .toString(36)
+      .substring(7)}`}
+    {...props}
+  />
+);
+
+const LatestPost = styled(LatestPostTemplate)`
+  margin-top: 1em !important;
+  margin-bottom: 1em !important;
+  width: auto !important;
+  max-width: ${1127 / 2 - 15}px !important;
+  @media (max-width: 1127px) {
+    max-width: 100% !important;
+    width: 100% !important;
   }
-}
+`;
+
+const Masonry = styled(MasonryTemplate)`
+  margin-top: -1em;
+  margin-bottom: -1em;
+`;
+
+const Gallery = () => (
+  <Masonry options={{ gutter: 30 }}>
+    <LatestPost as={Link} to={withPrefix("/")} />
+    <LatestPost as={Link} to={withPrefix("/")} />
+    <LatestPost as={Link} to={withPrefix("/")} />
+    <LatestPost as={Link} to={withPrefix("/")} />
+    <LatestPost as={Link} to={withPrefix("/")} />
+    <LatestPost as={Link} to={withPrefix("/")} />
+  </Masonry>
+);
+
+export default () => (
+  <Base noContainer>
+    <Container>
+      <Paper>
+        <Header
+          content="Featured Posts"
+          icon="star"
+          subheader={`Check out the featured, editorial posts below and get an insight into
+          recent news, updates and changes.`}
+        />
+      </Paper>
+    </Container>
+    <CoverflowSection />
+    <Container>
+      <Paper>
+        <Header
+          content="Latest Posts"
+          icon="newspaper"
+          subheader={`Non-editorial, federated posts by the community and/or core devs.
+          Consider contributing your own post by submitting a merge request!`}
+        />
+      </Paper>
+      <Gallery />
+    </Container>
+  </Base>
+);
