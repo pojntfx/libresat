c91e1386bf311c1af54b3c8f8874fe05d9703e33
Felix Pojtinger <felix@pojtinger.com>
2018-08-25 22:19:12 +0200

    feat(site): fetch blog posts in coverflow from MDX

--- packages/site/src/components/Blog/Coverflow.js
+++ packages/site/src/components/Blog/Coverflow.js
@@ -1,7 +1,27 @@
+import React from "react";
 import styled from "styled-components";
 import CoverflowTemplate from "reactjs-coverflow";
+import { Card } from "semantic-ui-react";
+import Link, { withPrefix } from "gatsby-link";
 
-export const Coverflow = styled(CoverflowTemplate)`
+const PostsCoverflow = styled(CoverflowTemplate)`
   margin-top: 1em;
   margin-bottom: 1em;
 `;
+
+const PostTemplate = ({ imgSrc, ...otherProps }) => (
+  <Card image={withPrefix(imgSrc)} {...otherProps} />
+);
+
+const Post = styled(PostTemplate)`
+  margin-top: 1em !important;
+  margin-bottom: 1em !important;
+`;
+
+export const Coverflow = ({ posts, ...otherProps }) => (
+  <PostsCoverflow rotate={10}>
+    {posts.map(({ link, ...props }, index) => (
+      <Post {...props} as={Link} to={link} key={index} />
+    ))}
+  </PostsCoverflow>
+);
