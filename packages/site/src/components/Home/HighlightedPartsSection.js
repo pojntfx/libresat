0cd086bab0cf40f8c798916257b299046d552a2a
Felix Pojtinger <felix@pojtinger.com>
2018-08-25 11:21:22 +0200

    feat(site): add highlighted parts section to home

---
+++ packages/site/src/components/Home/HighlightedPartsSection.js
@@ -0,0 +1,47 @@
+import React from "react";
+import { withPrefix, StaticQuery, graphql } from "gatsby";
+import { HighlightedParts } from "./HighlightedParts";
+
+const HighlightedPartsView = (
+  {
+    data: {
+      highlightedPartsYaml: { links, ...highlightedPartsYaml }
+    }
+  },
+  ...otherProps
+) => (
+  <HighlightedParts
+    links={links.map(({ link, ...rest }) => {
+      return {
+        link: withPrefix(link),
+        ...rest
+      };
+    })}
+    {...highlightedPartsYaml}
+    {...otherProps}
+  />
+);
+
+export const HighlightedPartsSection = props => (
+  <StaticQuery
+    query={graphql`
+      query HighlightedPartsQuery {
+        highlightedPartsYaml {
+          header {
+            content
+            subheader
+          }
+          description
+          linksPerRow
+          links {
+            label
+            icon
+            link
+            description
+          }
+        }
+      }
+    `}
+    render={data => <HighlightedPartsView data={data} {...props} />}
+  />
+);
--- packages/site/src/pages/index.js
+++ packages/site/src/pages/index.js
@@ -2,10 +2,12 @@ import React from "react";
 import { Base } from "../layouts/Base";
 import { HeroSection } from "../components/Home/HeroSection";
 import { SubsystemSection } from "../components/Home/SubsystemSection";
+import { HighlightedPartsSection } from "../components/Home/HighlightedPartsSection";
 
 export default () => (
   <Base>
     <HeroSection />
     <SubsystemSection />
+    <HighlightedPartsSection />
   </Base>
 );
