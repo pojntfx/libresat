d4e8257226854ed3fa2f3273e60ec5f38bdb170d
Felix Pojtinger <felix@pojtinger.com>
2018-08-26 02:58:28 +0200

    feat(site): add more meta tags to page

--- packages/site/gatsby-config.js
+++ packages/site/gatsby-config.js
@@ -24,6 +24,7 @@ module.exports = {
         path: `${__dirname}/src/posts/`
       }
     },
-    "gatsby-plugin-styled-components"
+    "gatsby-plugin-styled-components",
+    "gatsby-plugin-react-helmet"
   ]
 };
