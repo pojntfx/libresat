1f26e0985406ca9e0321de3d79fbc65673b155d7
Felix Pojtinger <felix@pojtinger.com>
2018-09-05 00:33:25 +0200

    refactor(service): implement mock resolver and typedef mergers, improve build setup and fix casting bugs

--- packages/service/src/servers/grapqhl/graphql.ts
+++ packages/service/src/servers/grapqhl/graphql.ts
@@ -1,16 +1,26 @@
-import { IGraphQL } from "./graphql.types";
+import { IGraphQL, IGraphQLParams } from "./graphql.types";
+import { GraphQLServer } from "graphql-yoga";
 
 class GraphQL implements IGraphQL {
   name: IGraphQL["name"];
   typeDefs: IGraphQL["typeDefs"];
   resolvers: IGraphQL["resolvers"];
   port: IGraphQL["port"];
+  server: IGraphQL["server"];
 
-  constructor({ name, typeDefs, resolvers, port }: IGraphQL) {
+  constructor({ name, typeDefs, resolvers, port }: IGraphQLParams) {
     this.name = name;
     this.typeDefs = typeDefs;
     this.resolvers = resolvers;
     this.port = port;
+  }
+
+  createServer() {
+    this.server = new GraphQLServer({
+      typeDefs: this.typeDefs,
+      resolvers: this.resolvers
+    });
+    return this.server;
   }
 }
 
--- packages/service/src/servers/grapqhl/graphql.types.ts
+++ packages/service/src/servers/grapqhl/graphql.types.ts
@@ -1,8 +1,22 @@
 import { IServer } from "../server.types";
+import { GraphQLServer } from "graphql-yoga";
+
+interface IGraphQLServerCreator {
+  (): GraphQLServer;
+}
 
 interface IGraphQL extends IServer {
+  typeDefs: string;
+  resolvers: any;
+  server: GraphQLServer;
+  createServer: IGraphQLServerCreator;
+}
+
+interface IGraphQLParams {
+  name: IServer["name"];
+  port: IServer["port"];
   typeDefs: string;
   resolvers: any[];
 }
 
-export { IGraphQL };
+export { IGraphQL, IGraphQLParams };
