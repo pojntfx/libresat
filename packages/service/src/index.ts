e9f035f39c49682f9c69bad02d58c8acb696f5c8
Felix Pojtinger <felix@pojtinger.com>
2018-09-04 23:41:26 +0200

    refactor(service): export and import from index

---
+++ packages/service/src/index.ts
@@ -0,0 +1,5 @@
+export { MongoDB } from "./databases/mongodb/mongodb";
+export { GraphQL } from "./servers/grapqhl/graphql";
+export { TypeDefMerger } from "./mergers/typedefs/typedefs";
+export { ResolverMerger } from "./mergers/resolvers/resolvers";
+export { composeService } from "./utils/composeService";
