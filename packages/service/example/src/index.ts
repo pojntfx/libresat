0da8283bceaeaac48f74ad00c42c3487a5318d38
Felix Pojtinger <felix@pojtinger.com>
2018-09-05 14:43:08 +0200

    feat(service): add mongodb implementation, improve services

--- packages/service/example/src/index.ts
+++ packages/service/example/src/index.ts
@@ -4,24 +4,23 @@ import {
   GraphQL,
   TypeDefMerger,
   ResolverMerger,
-  ModelMerger,
   Service
 } from "../../src/index";
 
-const {
-  services: [server, database]
-} = new Service("example-service", [
+const service = new Service("example-service", [
   new GraphQL(
     "example-service-server",
     new TypeDefMerger(loadPath(__dirname, "typeDefs")).merge(),
     new ResolverMerger(loadPath(__dirname, "resolvers")).merge(),
     3000
   ).start(),
-  new MongoDB({
-    name: "example-service-database",
-    models: new ModelMerger().merge(),
-    url: "mongodb://localhost:21700"
-  })
+  new MongoDB("example-service-database", "mongodb://localhost:27017").start()
 ]);
 
-console.log(server, database);
+console.log(<GraphQL>service.services[0]); // This is the service
+console.log(<MongoDB>service.services[1]); // This is the service
+
+const server = (<GraphQL>service.services[0]).server; // This is the actual server
+const database = (<MongoDB>service.services[1]).database; // This is the actual database
+
+export { service, server, database };
--- packages/service/src/databases/database.types.ts
+++ packages/service/src/databases/database.types.ts
@@ -1,8 +1,18 @@
 import { IService } from "../service/service.types";
 
+interface IDatabaseConnector {
+  (): any;
+}
+
+interface IDatabaseStarter {
+  (): any;
+}
 interface IDatabase extends IService {
   name: string;
   url: string;
+  database: any;
+  connect: IDatabaseConnector;
+  start: IDatabaseStarter;
 }
 
 export { IDatabase };
