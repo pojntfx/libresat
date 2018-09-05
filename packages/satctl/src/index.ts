77cad3e5a6d68620614ba24a1164597df19826d8
Felix Pojtinger <felix@pojtinger.com>
2018-09-05 22:35:30 +0200

    feat(satctl): add basic YAML parser as starter for implementing satctl

---
+++ packages/satctl/src/index.ts
@@ -0,0 +1,35 @@
+import { Command, flags } from "@oclif/command";
+import { loadFile } from "./utils";
+const { version, help } = flags;
+
+/**
+ * Main command
+ */
+class Main extends Command {
+  static description = `"kubectl"-like CLI to develop or/and control satellites using the LibreSat Platform (https://libresat.space).`;
+
+  static flags = {
+    version: version({ char: "v" }),
+    help: help({ char: "h" })
+  };
+
+  static args = [
+    {
+      name: "file"
+    }
+  ];
+
+  async run() {
+    const {
+      args: { file }
+    } = super.parse(Main);
+
+    if (file) {
+      console.log(JSON.stringify(loadFile(file), null, 2));
+    } else {
+      super.warn("Please provide a YAML file to parse.");
+    }
+  }
+}
+
+export { Main };
