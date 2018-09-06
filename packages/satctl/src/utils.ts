36313e8a4ee070de120b3bd6648aa419a9578a79
Felix Pojtinger <felix@pojtinger.com>
2018-09-06 04:14:50 +0200

    feat(satctl): implement check for whether file exists

--- packages/satctl/src/utils.ts
+++ packages/satctl/src/utils.ts
@@ -1,6 +1,7 @@
 import { safeLoad } from "js-yaml";
-import { readFileSync } from "fs";
+import { existsSync, readFileSync } from "fs";
 
-const loadFile = (path: string) => safeLoad(readFileSync(path, "utf-8"));
+const loadFile = (path: string) =>
+  safeLoad(existsSync(path) ? readFileSync(path, "utf-8") : false);
 
 export { loadFile };
