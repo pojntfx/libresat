65f374079354606a7e94a5a22e898543c4a4c7a1
Felix Pojtinger <felix@pojtinger.com>
2018-09-07 23:39:08 +0200

    feat(service): enable port and dbUrl configuration at start method

--- packages/service/src/predefined/graphQLMongoDB/graphQLMongoDB.ts
+++ packages/service/src/predefined/graphQLMongoDB/graphQLMongoDB.ts
@@ -30,15 +30,18 @@ class GraphQLMongoDB implements IGraphQLMongoDB {
     });
   }
 
-  start() {
+  start(
+    port?: IGraphQLMongoDBParams["port"],
+    dbUrl?: IGraphQLMongoDBParams["dbUrl"]
+  ) {
     this.service = new Service(this.name, [
       new GraphQL(
         `${this.name}-server`,
         new TypeDefMerger(this.typeDefDir).merge(),
         new ResolverMerger(this.resolverDir).merge(),
-        this.port
+        port ? port : this.port
       ).start(),
-      new MongoDB(`${this.name}-database`, this.dbUrl).start()
+      new MongoDB(`${this.name}-database`, dbUrl ? dbUrl : this.dbUrl).start()
     ]).start();
     const server = <GraphQLServer>(<GraphQL>this.service.services[0]).server;
     const database = <Mongoose>(<MongoDB>this.service.services[1]).database;
