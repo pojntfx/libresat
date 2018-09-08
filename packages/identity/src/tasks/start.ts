1485ffdaea8b7834f87b61598198c26412b76af2
Felicitas Pojtinger <felicitas@pojtinger.com>
2018-09-08 03:21:56 +0200

    feat(identity): add simple help text

--- packages/identity/src/tasks/start.ts
+++ packages/identity/src/tasks/start.ts
@@ -2,15 +2,27 @@ import { service } from "../index";
 
 const args = process.argv.slice(2);
 
-const port: number =
-  parseInt(args[args.indexOf("--port") + 1]) ||
-  parseInt(args[args.indexOf("-p") + 1]);
+if (args.includes("-h") || args.includes("--help")) {
+  console.log(`LibreSat Identity
+@libresat/identity#0.0.1-10
 
-const dbUrl: string =
-  args[args.indexOf("--db-url") + 1] ||
-  args[args.indexOf("--database-url") + 1];
+Usage: identity [OPTION...]
 
-service.start(
-  !isNaN(port) ? port : 3000,
-  /mongodb:\/\/.*:\d+/.test(dbUrl) ? dbUrl : "mongodb://localhost:27017"
-);
+  -p,       --port          Port to run the GraphQL server on
+  --db-url, --database-url  MongoDB server to connect to
+
+More info: https://libresat.space/docs/services/identity`);
+} else {
+  const port: number =
+    parseInt(args[args.indexOf("--port") + 1]) ||
+    parseInt(args[args.indexOf("-p") + 1]);
+
+  const dbUrl: string =
+    args[args.indexOf("--db-url") + 1] ||
+    args[args.indexOf("--database-url") + 1];
+
+  service.start(
+    !isNaN(port) ? port : 3000,
+    /mongodb:\/\/.*:\d+/.test(dbUrl) ? dbUrl : "mongodb://localhost:27017"
+  );
+}
