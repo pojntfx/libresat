92d842c4bb0740190e5997b8e9788f6a00b0bead
Felicitas Pojtinger <felicitas@pojtinger.com>
2018-09-12 18:10:43 +0200

    feat(identity): implement WIP atomic role resolver

--- packages/identity/src/controllers/organization.controller.ts
+++ packages/identity/src/controllers/organization.controller.ts
@@ -1,14 +1,5 @@
 import { GraphQLMongoDBController as Controller } from "@libresat/service";
 
-class OrganizationController extends Controller {
-  async get(id: string) {
-    return this.model.findById(id).populate({
-      path: "roles",
-      populate: {
-        path: "users"
-      }
-    });
-  }
-}
+class OrganizationController extends Controller {}
 
 export { OrganizationController };
