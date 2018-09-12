c17751187716ab411a16ea6c072cd2cf9fc2821a
Felix Pojtinger <felix@pojtinger.com>
2018-09-12 18:10:43 +0200

    feat(identity): implement WIP atomic role resolver

--- packages/identity/src/resolvers/organization.resolver.ts
+++ packages/identity/src/resolvers/organization.resolver.ts
@@ -1,5 +1,6 @@
 import { OrganizationController } from "../controllers/organization.controller";
 import { OrganizationModel } from "../models/organization.model";
+import { role } from "./role.resolver";
 
 const organization = new OrganizationController(OrganizationModel);
 
@@ -9,8 +10,14 @@ export default {
       await organization.create(params)
   },
   Query: {
-    getOrganization: async (_: any, params: any) =>
+    organization: async (_: any, params: any) =>
       await organization.get(params.id)
+  },
+  Organization: {
+    roles: async (parent: any, params: any) =>
+      await organization
+        .get(parent.id)
+        .then(org => org.roles.map((id: string) => role.get(id)))
   }
 };
 
