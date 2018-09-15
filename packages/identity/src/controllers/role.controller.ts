baf5d01b2927420046413f23acb63799270108aa
Felicitas Pojtinger <felicitas@pojtinger.com>
2018-09-15 19:42:57 +0200

    feat(identity): add simple mock resolvers and controllers

--- packages/identity/src/controllers/role.controller.ts
+++ packages/identity/src/controllers/role.controller.ts
@@ -1,37 +1,5 @@
 import { GraphQLMongoDBController as Controller } from "@libresat/service";
-import { organization } from "../resolvers/organization.resolver";
-import { user } from "../resolvers/user.resolver";
 
-class RoleController extends Controller {
-  async create(params: any) {
-    // Create role
-    const newRole = await super.create({
-      name: params.name
-    });
-    // Get organization to which this role belongs to
-    const ownerOrganization = await organization.get(params.organizationId);
-    // Assign role to organization
-    newRole.organization = ownerOrganization;
-    // Save the role
-    await newRole.save();
-    // Assign organization to role
-    ownerOrganization.roles.push(newRole);
-    // Save the organization
-    await ownerOrganization.save();
-    return newRole;
-  }
-
-  getOrganization = async (parent: any) =>
-    await this.get(parent.id).then(
-      async (role: any) => await organization.get(role.organization._id)
-    );
-
-  getAllUsers = async (parent: any) =>
-    await this.get(parent.id).then(async (role: any) =>
-      role.users.map((userId: string) => user.get(userId))
-    );
-
-  filter = async (params: any) => super.filter(params.filter || undefined);
-}
+class RoleController extends Controller {}
 
 export { RoleController };
--- packages/identity/src/resolvers/organization.resolver.ts
+++ packages/identity/src/resolvers/organization.resolver.ts
@@ -4,23 +4,12 @@ import { OrganizationModel } from "../models/organization.model";
 const organization = new OrganizationController(OrganizationModel);
 
 export default {
+  Organization: {},
   Mutation: {
-    createOrganization: async (_: any, params: any) =>
-      await organization.create(params),
-    updateOrganization: async (_: any, params: any, context: any) =>
-      await organization.update(params.id, params),
-    deleteOrganization: async (_: any, params: any) =>
-      await organization.delete(params.id),
-    deleteAllOrganizations: async () => await organization.deleteAll()
-  },
-  Query: {
-    organization: async (_: any, params: any) =>
-      await organization.get(params.id),
-    organizations: async (_: any, params: any) =>
-      await organization.filter(params)
-  },
-  Organization: {
-    roles: async (parent: any) => await organization.getAllRoles(parent)
+    assignUserToOrganization: async (_: any, params: any) =>
+      await organization.assignUser(params),
+    assignRoleToOrganization: async (_: any, params: any) =>
+      await organization.assignRole(params)
   }
 };
 
