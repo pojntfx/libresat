60234bc42ff682e7d3d1213f2f8737fa7104c943
Felix Pojtinger <felix@pojtinger.com>
2018-09-15 19:42:57 +0200

    feat(identity): add simple mock resolvers and controllers

--- packages/identity/src/controllers/organization.controller.ts
+++ packages/identity/src/controllers/organization.controller.ts
@@ -1,34 +1,16 @@
 import { GraphQLMongoDBController as Controller } from "@libresat/service";
-import { role } from "../resolvers/role.resolver";
 
 class OrganizationController extends Controller {
-  getAllRoles = async (parent: any) =>
-    await this.get(parent.id).then(org =>
-      org.roles.map(async (roleId: string) => await role.get(roleId))
+  async assignUser(params: any) {
+    console.log(
+      `Assigning user ${params.userId} to organization ${params.organizationId}`
     );
+  }
 
-  filter = async (params: any) => super.filter(params.filter || undefined);
-
-  getAllByName = async (name: string) =>
-    await this.model
-      .find({
-        name
-      })
-      .populate("roles");
-
-  async filterRolesByNames(name: string, roleNames: [string]) {
-    const orgsThatMatchName = await this.getAllByName(name);
-    let authorizedRoles = [];
-    for (let roleName of roleNames) {
-      for (let org of orgsThatMatchName) {
-        for (let role of org.roles) {
-          if (role.name === roleName) {
-            authorizedRoles.push(role);
-          }
-        }
-      }
-    }
-    return authorizedRoles;
+  async assignRole(params: any) {
+    console.log(
+      `Assigning role ${params.roleId} to organization ${params.organizationId}`
+    );
   }
 }
 
--- packages/identity/src/resolvers/user.resolver.ts
+++ packages/identity/src/resolvers/user.resolver.ts
@@ -4,28 +4,13 @@ import { UserModel } from "../models/user.model";
 const user = new UserController(UserModel);
 
 export default {
-  Mutation: {
-    createUser: async (_: any, params: any) => await user.create(params),
-    updateUser: async (_: any, params: any, context: any) =>
-      await user.update(params.userId, {
-        context,
-        ...params
-      }),
-    deleteUser: async (_: any, _2: any, context: any) =>
-      await user.delete({ context }),
-    deleteAllUsers: async () => await user.deleteAll(),
-    authorizeByOrgAndRoles: async (_: any, params: any, context: any) =>
-      await user.authorizeByOrgAndRoles({
-        context,
-        ...params
-      })
-  },
   Query: {
-    user: async (_: any, params: any) => await user.get(params.id),
-    users: async (_: any, params: any) => await user.filter(params)
+    hello: async () => "Hello, world!"
   },
-  User: {
-    roles: async (parent: any) => await user.getAllRoles(parent)
+  User: {},
+  Mutation: {
+    assignRoleToUser: async (_: any, params: any) =>
+      await user.assignRole(params)
   }
 };
 
