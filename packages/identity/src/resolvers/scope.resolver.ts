6d01afc6cb71ae45e3300dd38064c99f3de16037
Felix Pojtinger <felix@pojtinger.com>
2018-09-16 17:46:30 +0200

    feat(identity): add resolvers for scope

--- packages/identity/src/resolvers/scope.resolver.ts
+++ packages/identity/src/resolvers/scope.resolver.ts
@@ -4,14 +4,20 @@ import { ScopeModel } from "../models/scope.model";
 const scope = new ScopeController(ScopeModel);
 
 export default {
-  Scope: {},
+  Query: {
+    scopes: async () => scope.getAll(),
+    scope: async (_: any, params: any) => scope.get(params.id)
+  },
+  Scope: {
+    users: async (parent: any) => await scope.getAllUsers(parent),
+    roles: async (parent: any) => await scope.getAllRoles(parent)
+  },
   Mutation: {
     assignUserToScope: async (_: any, params: any) =>
       await scope.assignUser(params),
     assignRoleToScope: async (_: any, params: any) =>
       await scope.assignRole(params),
-    createScope: async (_: any, params: any) =>
-      await scope.create(params)
+    createScope: async (_: any, params: any) => await scope.create(params)
   }
 };
 
--- packages/identity/src/resolvers/user.resolver.ts
+++ packages/identity/src/resolvers/user.resolver.ts
@@ -4,9 +4,6 @@ import { UserModel } from "../models/user.model";
 const user = new UserController(UserModel);
 
 export default {
-  Query: {
-    hello: async () => "Hello, world!"
-  },
   User: {},
   Mutation: {
     assignRoleToUser: async (_: any, params: any) =>
