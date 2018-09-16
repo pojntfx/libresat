0fc540c53c97c00c8a1a3c2622d556dc4b370959
Felicitas Pojtinger <felicitas@pojtinger.com>
2018-09-16 17:59:19 +0200

    feat(identity): add resolvers for role

--- packages/identity/src/controllers/role.controller.ts
+++ packages/identity/src/controllers/role.controller.ts
@@ -1,5 +1,11 @@
 import { GraphQLMongoDBController as Controller } from "@libresat/service";
 
-class RoleController extends Controller {}
+class RoleController extends Controller {
+  getAllUsers = async (parent: any) =>
+    (await this.model.findById(parent.id).populate("users")).users;
+
+  getAllScopes = async (parent: any) =>
+    (await this.model.findById(parent.id).populate("scopes")).scopes;
+}
 
 export { RoleController };
--- packages/identity/src/resolvers/role.resolver.ts
+++ packages/identity/src/resolvers/role.resolver.ts
@@ -4,7 +4,14 @@ import { RoleModel } from "../models/role.model";
 const role = new RoleController(RoleModel);
 
 export default {
-  Role: {},
+  Role: {
+    users: async (parent: any) => await role.getAllUsers(parent),
+    scopes: async (parent: any) => await role.getAllScopes(parent)
+  },
+  Query: {
+    roles: async () => await role.getAll(),
+    role: async (_: any, params: any) => await role.get(params.id)
+  },
   Mutation: {
     createRole: async (_: any, params: any) => await role.create(params)
   }
