c718f0b9bba3aec2dd14b05aab4e015e27e9daab
Felix Pojtinger <felix@pojtinger.com>
2018-10-08 18:42:09 +0200

    feat(identity-frontend): add background image

--- packages/identity-frontend/src/components/layouts/Default.layout.tsx
+++ packages/identity-frontend/src/components/layouts/Default.layout.tsx
@@ -3,6 +3,15 @@ import "semantic-ui-css/semantic.min.css";
 import { Navbar, Container } from "../global";
 import { Footer } from "../global/Footer/Footer";
 import { IDefaultLayoutProps } from "../../types";
+import { injectGlobal } from "styled-components";
+import bg from "../../assets/bg.jpg";
+
+injectGlobal`
+  body {
+    background: url(${bg}) no-repeat center center fixed;
+    background-size: cover;
+  }
+`;
 
 const DefaultLayout = ({
   navbar,
