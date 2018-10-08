cba56a1ee7e3475bc4a30e6a186c17a65b7d9bb7
Felicitas Pojtinger <felicitas@pojtinger.com>
2018-10-08 15:38:58 +0200

    feat(identity-frontend): add top and bottom margins to container

---
+++ packages/identity-frontend/src/components/global/Container.ts
@@ -0,0 +1,9 @@
+import styled from "styled-components";
+import { Container as ContainerTemplate } from "semantic-ui-react";
+
+const Container = styled(ContainerTemplate)`
+  margin-top: 1em;
+  margin-bottom: 1em;
+`;
+
+export { Container };
