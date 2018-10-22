bc16a0586936efb2095f0f5d2bcb87006e2fd65f
Felix <felix@pojtinger.com>
2018-10-21 18:45:39 -0700

    feat(frontend-components): Add Button from identity-frontend

---
+++ packages/frontend-components/src/components/Button.ts
@@ -0,0 +1,8 @@
+import { Button as ButtonTemplate } from "semantic-ui-react";
+import styled from "react-emotion";
+
+const Button = styled(ButtonTemplate)`
+  white-space: nowrap;
+`;
+
+export { Button };
